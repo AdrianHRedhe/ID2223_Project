525a7ac1c8a3ba6de691952c90bd56b284ad7b98
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2024-01-07 23:05:16 +0100

    feat: Add file that quickly checks the ip and switches it

---
+++ data_collection_process/scrape_files_for_container/switch_ip.py
@@ -0,0 +1,19 @@
+import mybrowser
+import time
+import sys
+import requests
+from datetime import datetime 
+
+# Set the encoding for stdout to UTF-8
+#sys.setdefaultencoding('utf-8')
+
+start_time = datetime.now()
+response = requests.get('http://icanhazip.com/', proxies={'http': '127.0.0.1:8118'})
+print(response.text.strip(), datetime.now()-start_time)
+print()
+
+mybrowser.MyBrowserClass.set_new_ip()
+time.sleep(2)
+
+response = requests.get('http://icanhazip.com/', proxies={'http': '127.0.0.1:8118'})
+print(response.text.strip(), datetime.now()-start_time)
